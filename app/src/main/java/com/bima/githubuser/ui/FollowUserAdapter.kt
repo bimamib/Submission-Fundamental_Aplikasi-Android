772bbc8bd1fcd4d207c3e8276927f293d082977f
bimamib <bimaprast1601@gmail.com>
2023-09-17 06:25:37 +0700

    submission 1 done

---
+++ app/src/main/java/com/bima/githubuser/ui/FollowUserAdapter.kt
@@ -0,0 +1,54 @@
+package com.bima.githubuser.ui
+
+import android.content.Intent
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.bima.githubuser.data.response.ItemsItem
+import com.bima.githubuser.databinding.ListUserBinding
+import com.bima.githubuser.description.DetailActivity
+import com.bumptech.glide.Glide
+
+class FollowUserAdapter : ListAdapter<ItemsItem, FollowUserAdapter.MyViewHolder>(DIFF_CALLBACK) {
+    class MyViewHolder(private val binding: ListUserBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(item_name: ItemsItem) {
+            binding.tvItemName.text = item_name.login
+            Glide.with(binding.root)
+                .load(item_name.avatarUrl)
+                .into(binding.imgItemPhoto)
+            binding.root.setOnClickListener {
+                val intentDetail = Intent(binding.root.context, DetailActivity::class.java)
+                intentDetail.putExtra("ID", item_name.id)
+                intentDetail.putExtra("USERNAME", item_name.login)
+                intentDetail.putExtra("AVATAR", item_name.avatarUrl)
+                binding.root.context.startActivity(intentDetail)
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        val binding = ListUserBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return MyViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        val item = getItem(position)
+        holder.bind(item)
+    }
+
+    companion object {
+        val DIFF_CALLBACK = object : DiffUtil.ItemCallback<ItemsItem>() {
+            override fun areItemsTheSame(oldItem: ItemsItem, newItem: ItemsItem): Boolean {
+                return oldItem == newItem
+            }
+
+            override fun areContentsTheSame(oldItem: ItemsItem, newItem: ItemsItem): Boolean {
+                return oldItem == newItem
+            }
+        }
+    }
+
+}
---
+++ app/src/main/java/com/bima/githubuser/ui/SectionPagerAdapter.kt
@@ -0,0 +1,23 @@
+package com.bima.githubuser.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.bima.githubuser.description.FollowFragment
+
+class SectionPagerAdapter(activity: AppCompatActivity, private val username: String) :
+    FragmentStateAdapter(activity) {
+    override fun createFragment(position: Int): Fragment {
+        val fragment = FollowFragment()
+        fragment.arguments = Bundle().apply {
+            putInt(FollowFragment.ARG_POSITION, position + 1)
+            putString(FollowFragment.ARG_USERNAME, username)
+        }
+        return fragment
+    }
+
+    override fun getItemCount(): Int {
+        return 2
+    }
+}
