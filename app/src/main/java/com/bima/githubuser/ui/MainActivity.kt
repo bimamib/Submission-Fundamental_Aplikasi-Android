04eb39190be15cbe73ea374ae7c2b0d75ef9e5d9
bimamib <bimaprast1601@gmail.com>
2023-09-28 06:27:13 +0700

    menambah font

--- app/src/main/java/com/bima/githubuser/ui/MainActivity.kt
+++ app/src/main/java/com/bima/githubuser/ui/MainActivity.kt
@@ -3,6 +3,8 @@ package com.bima.githubuser.ui
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.view.View
 import androidx.activity.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -16,6 +18,7 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     private val userViewModel by viewModels<UserViewModel>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
